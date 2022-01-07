8fca9b05e614bddbfb05e0789413e49c0fa5993c
Vladimir Fomchenkov <v.fom4enkov@gmail.com>
2022-01-07 23:50:24 +0300

    Add BuildItem tests

---
+++ test/test_build_item.h
@@ -0,0 +1,15 @@
+#ifndef CPP_TOOL_KIT_FACTORY_TEST_BUILD_ITEM_H_
+#define CPP_TOOL_KIT_FACTORY_TEST_BUILD_ITEM_H_
+
+#include <boost/test/unit_test.hpp>
+
+// just for tests
+#define private public
+#define protected public
+
+#include <cpptoolkit/factory/core_extension.h>
+#include <cpptoolkit/factory/tool/build_item.h>
+
+#include "mock/fixture.h"
+
+#endif  // CPP_TOOL_KIT_FACTORY_TEST_BUILD_ITEM_H_
