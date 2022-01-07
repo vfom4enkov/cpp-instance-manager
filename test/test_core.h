cb82469f969c7ec2039a82f2745e842c597f0a08
Vladimir Fomchenkov <v.fom4enkov@gmail.com>
2022-01-07 23:48:01 +0300

    Add Core tests

---
+++ test/test_core.h
@@ -0,0 +1,15 @@
+	#ifndef CPP_TOOL_KIT_FACTORY_TEST_CORE_H_
+#define CPP_TOOL_KIT_FACTORY_TEST_CORE_H_
+
+#include <boost/test/unit_test.hpp>
+
+// just for tests
+#define private public
+#define protected public
+
+#include <cpptoolkit/factory/core_extension.h>
+#include <cpptoolkit/factory/manager/multiple_instance_manager.h>
+
+#include "mock/fixture.h"
+
+#endif  // CPP_TOOL_KIT_FACTORY_TEST_CORE_H_
