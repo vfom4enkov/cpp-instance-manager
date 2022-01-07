a907152db71d387442e4d4b4edfb27a0b2d109de
Vladimir Fomchenkov <v.fom4enkov@gmail.com>
2022-01-07 17:09:44 +0300

    Add Lock pool manager

---
+++ src/lock_pool_instance_manager.h
@@ -0,0 +1,99 @@
+#ifndef CPP_TOOL_KIT_FACTORY_LOCK_POOL_INSTANCE_MANAGER_H_
+#define CPP_TOOL_KIT_FACTORY_LOCK_POOL_INSTANCE_MANAGER_H_
+
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+#include <unordered_map>
+
+#include "base_instance_manager.h"
+#include "pool_context.h"
+
+namespace cpptoolkit {
+namespace factory {
+
+/// Locks the thread until an instance was put back to the pool (if it is empty)
+/// @tparam T Type of managed object
+template <typename T>
+class LockPoolInstanceManager : public BaseInstanceManager<T>,
+                                public AbstractPoolInstancePutback {
+ public:
+  /// @brief Create instance manager
+  /// @param [in] class_name_key - Unique key for current manager
+  /// @param [in] create - Function for create instance of managed object
+  /// @param [in] core - Pointer to the core_ with registered objects
+  /// @param [in] pool_size - Size of pool object
+  LockPoolInstanceManager(std::string class_name_key,
+                          std::function<T*(Resolver&)>&& create,
+                          Core* core, uint32_t pool_size) noexcept
+      : BaseInstanceManager(class_name_key, std::move(create), core),
+        countdown_(pool_size),
+        waiter_counter_(0){};
+
+  virtual ~LockPoolInstanceManager() noexcept = default;
+
+  std::unique_ptr<BaseContext<T>> Get() noexcept override;
+  void Callback(uintptr_t key) noexcept override;
+
+ private:
+  uint32_t countdown_;       // counter of objects what will be created for the pool
+  uint32_t waiter_counter_;  // size of waiting threads
+
+  std::queue<uintptr_t> queue_;  // free objects in the pool
+  std::unordered_map<uintptr_t, std::unique_ptr<Context<T>>> index_;  // all created pool objects
+
+  std::condition_variable queue_cv_;
+  std::mutex mutex_;
+};
+
+template <typename T>
+inline std::unique_ptr<BaseContext<T>>
+LockPoolInstanceManager<T>::Get() noexcept {
+  std::unique_lock<std::mutex> locker(mutex_);
+
+  if (countdown_ > 0 && queue_.empty()) {
+    // create object for the pool
+    std::unique_ptr<Context<T>> context = MakeUnique<Context<T>>();
+    Create(context.get());
+
+    if (!context->IsValid()) {
+      return context;
+    }
+
+    uintptr_t context_key = reinterpret_cast<uintptr_t>(context.get());
+    std::unique_ptr<PoolContext<T>> pool_ctx =
+        MakeUnique<PoolContext<T>>(this, context.get(), context_key);
+    index_.emplace(context_key, std::move(context));
+    --countdown_;
+    return pool_ctx;
+  }
+
+  if (queue_.empty()) {
+    // need to wait when one instance will be free
+    ++waiter_counter_;
+    queue_cv_.wait(locker, [&]() { return !queue_.empty(); });
+    --waiter_counter_;
+  }
+
+  uintptr_t key = queue_.front();
+  queue_.pop();
+  const auto it = index_.find(key);
+  std::unique_ptr<Context<T>>& context = it->second;
+  std::unique_ptr<PoolContext<T>> pool_context =
+      MakeUnique<PoolContext<T>>(this, context.get(), key);
+  return pool_context;
+}
+
+template <typename T>
+inline void LockPoolInstanceManager<T>::Callback(uintptr_t key) noexcept {
+  std::unique_lock<std::mutex> locker(mutex_);
+  queue_.push(key);
+  if (waiter_counter_ > 0) {
+    queue_cv_.notify_one();
+  }
+}
+
+}  // namespace factory
+}  // namespace cpptoolkit
+
+#endif  // CPP_TOOL_KIT_FACTORY_LOCK_POOL_INSTANCE_MANAGER_H_
---
+++ src/pool_context.h
@@ -0,0 +1,50 @@
+#ifndef CPP_TOOL_KIT_FACTORY_POOL_CONTEXT_H_
+#define CPP_TOOL_KIT_FACTORY_POOL_CONTEXT_H_
+
+#include "context.h"
+
+namespace cpptoolkit {
+namespace factory {
+
+/// @brief Observer for return object back to the pool
+class AbstractPoolInstancePutback {
+ public:
+  virtual ~AbstractPoolInstancePutback() noexcept = default;
+
+  /// @brief Event notification, to put back object to the pool
+  /// @param key - Index key of managed object
+  virtual void Callback(uintptr_t key) noexcept = 0;
+};
+
+/// @brief Contains info about instance from the pool
+/// @tparam T - Type of managed object
+template <typename T>
+class PoolContext : public BaseContext<T> {
+ public:
+  /// @brief Create context
+  /// @param putback - Pointer to observer
+  /// @param context - Pointer to context (with object instance and validation
+  /// state)
+  PoolContext(AbstractPoolInstancePutback* putback, Context<T>* ctx, uintptr_t key) noexcept
+      : BaseContext<T>(true), putback_(putback), key_(key) {
+    instance_ptr_ = ctx->GetInstance();
+  };
+
+  ~PoolContext() noexcept;
+
+ private:
+  AbstractPoolInstancePutback* putback_;
+  uintptr_t key_;
+};
+
+// Implementation
+
+template <typename T>
+PoolContext<T>::~PoolContext() noexcept {
+  putback_->Callback(key_);
+}
+
+}  // namespace factory
+}  // namespace cpptoolkit
+
+#endif  // CPP_TOOL_KIT_FACTORY_POOL_CONTEXT_H_
