057ca7ce23b3e05cbab7fee2a5ac8e256a58dc25
Vladimir Fomchenkov <v.fom4enkov@gmail.com>
2022-01-16 20:31:05 +0300

    Add types description

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # CppToolKit-Factory
-The tool provides control of lifetime for business logic C++ objects.
+Header based tool provides control of lifetime for C++ business logic objects.
 
 ## How to use
 
@@ -9,15 +9,28 @@ Download the tool and add the path to `src` to the `PATH`. Or if you use `cmake`
 include_directories({pat_to_cpptoolkit-factory}/src)
 ```
 
+### Type of objects
+There are available four types:
+- *Single* - an instance created once and used many times
+- *Multiple* - (default type) an instance is created every time on request
+- *Lock pool* - an instance is created if the pool is not full or the thread is blocked until another instance returns to the pool
+- *Soft pool* - an instance is created if the pool is empty. After the instance returns to the pool it is deleted if the pool is full
+
 ### Register objects
 If an object without dependencies:
 ```
 #include <cpptoolkit/factory/builder.h>
 ...
-namespace cf = cpptoolkit::factory;
-...
-cf::Builder builder;
+cpptoolkit::factory::Builder builder;
 RegisterType<my_object>();
 ```
 
 Object with dependencies:
+```
+#include <cpptoolkit/factory/builder.h>
+...
+namespace cf = cpptoolkit::factory;
+...
+cpptoolkit::factory::Builder builder;
+RegisterType<my_object>();
+```
--- example/registrator.cpp
+++ example/registrator.cpp
@@ -7,8 +7,8 @@
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
@@ -16,14 +16,15 @@
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include "registrator.h"
@@ -36,11 +37,10 @@
 #include "root.h"
 #include "single.h"
 
+namespace example {
 namespace cf = cpptoolkit::factory;
 
-namespace example {
-
-std::unique_ptr<cpptoolkit::factory::Core> RegisterObjects(std::string& error) {
+std::unique_ptr<cf::Core> RegisterObjects(std::string& error) {
   cf::Builder builder;
   builder
       .Register<Abstract>([](cf::Resolver& resolver) -> Abstract* {
